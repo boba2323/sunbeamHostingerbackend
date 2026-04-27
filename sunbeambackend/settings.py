18c33e5bb46cb807752e6c3ccd24928bd0e5ceae
boba2323 <rajkumarmech95@gmail.com>
2026-04-27 20:30:33 +0530

    emails tls 2

--- sunbeambackend/settings.py
+++ sunbeambackend/settings.py
@@ -166,7 +166,8 @@ DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
 DEFAULT_FROM_EMAIL = "deadryefield@gmail.com"
 
 EMAIL_HOST = "smtp.gmail.com"
-EMAIL_PORT = 587
+EMAIL_PORT = 465
+EMAIL_USE_SSL = True
 EMAIL_USE_TLS = False
 
 EMAIL_HOST_USER = "rajkumarmech95@gmail.com"
