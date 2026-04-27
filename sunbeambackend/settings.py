80355f383ee6562ea47d5da0a49c914d446a8a52
boba2323 <rajkumarmech95@gmail.com>
2026-04-27 20:28:16 +0530

    emails tls

--- sunbeambackend/settings.py
+++ sunbeambackend/settings.py
@@ -167,7 +167,7 @@ DEFAULT_FROM_EMAIL = "deadryefield@gmail.com"
 
 EMAIL_HOST = "smtp.gmail.com"
 EMAIL_PORT = 587
-EMAIL_USE_TLS = True
+EMAIL_USE_TLS = False
 
 EMAIL_HOST_USER = "rajkumarmech95@gmail.com"
 
